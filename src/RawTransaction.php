95a47f24f82f786d3f4bccfec14837b044182767
h-byte-hegelian <34963473+h-byte-hegelian@users.noreply.github.com>
2018-12-12 17:42:42 +0300

    Update RawTransaction.php

--- src/RawTransaction.php
+++ src/RawTransaction.php
@@ -595,9 +595,9 @@ class RawTransaction
         $info = array();
         $info['txid'] = $txid;
         $info['version'] = $math->hexDec(self::_return_bytes($raw_transaction, 4, true));
-        if (!in_array($info['version'], array('0', '1'))) {
-            throw new \InvalidArgumentException("Invalid transaction version");
-        }
+        //if (!in_array($info['version'], array('0', '1'))) {
+        //    throw new \InvalidArgumentException("Invalid transaction version");
+        //}
 
         $input_count = self::_get_vint($raw_transaction);
         if (!($input_count >= 0 && $input_count <= 4294967296)) {
